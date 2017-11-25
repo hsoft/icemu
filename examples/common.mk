6dfb074a72bfa08b6793823a59f30a6df7edb052
Virgil Dupras <hsoft@hardcoded.net>
2017-11-25 14:17:54 -0500

    Fix make invocation in examples

--- examples/common.mk
+++ examples/common.mk
@@ -40,7 +40,7 @@ $(PROGNAME).hex: $(PROGNAME).bin
 
 # we always want to run make on the ../.. to make sure we have a fresh lib.
 $(PROGNAME): $(SIM_OBJS)
-	gmake -C ../..
+	$(MAKE) -C ../..
 	$(CC) `pkg-config --libs --static ncurses` -L../.. $(SIM_OBJS) -o $@ -licemu
 
 sim: CC = cc
